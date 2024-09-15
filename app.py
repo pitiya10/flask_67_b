12722ca093e3f60b52a6c447e799bdb9c1b52789
Pitiya <6749010036@phuketvc.ac.th>
2024-09-15 15:26:29 +0700

    9/15

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ DB_USER = "root"
 DB_PASS = ""
 DB_NAME = "flower_shop"
 
+# For Session
+app.config['SECRET_KEY'] = "0901587761z"
+
+
 @app.route("/home")
 def home():
    # return "<h1>Hello world</h1>"
@@ -45,9 +49,119 @@ def store():
         my_cursor.execute(sql, val)
         my_db.commit()
 
-        return redirect("/create")
+        session['alert_status'] = "success"
+        session['alert_message'] = "Already Created!"
+        return redirect("/")
     else:
-        return "<h1>No Way!</h1>"
+        session['alert_status'] = "fail"
+        session['alert_message'] = "Something went wrong!"
+        return redirect("/")
+
+@app.route("/")
+def index():
+     # Connect Database
+        my_db = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME
+        )
+        my_cursor = my_db.cursor(dictionary=True)
+        sql = "SELECT * FROM flowers"
+        my_cursor.execute(sql)
+        results = my_cursor.fetchall()
+
+        # Show Message
+        if "alert_status" in session and "alert_message" in session:
+             alert_message = {
+                  'status': session['alert_status'],
+                  'message': session['alert_message'],
+             }
+             del session['alert_status']
+             del session['alert_message']
+        else:
+            
+             alert_message = {
+             'status': None,
+             'message': None,
+        }
+
+        return render_template("index.html", alert_message = alert_message, results = results)
+
+@app.route("/edit/<flower_id>", methods=["GET"])
+def edit(flower_id):
+    # Connect Database
+        my_db = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME
+        )
+        my_cursor = my_db.cursor(dictionary=True)
+        sql = "SELECT * FROM flowers WHERE id = " + flower_id
+        my_cursor.execute(sql)
+        results = my_cursor.fetchall()
+
+        return render_template("edit.html", results = results)
+
+
+@app.route("/update/<flower_id>", methods=["POST"])
+def update(flower_id):
+     if request.method == "POST":
+        flower_name = request.form['flower_name']
+        lat_num = request.form['lat_num']
+        long_num = request.form['long_num']
+        place = request.form['place']
+        detail = request.form['detail']
+        print("input:", flower_name, lat_num, long_num, place, detail)
+
+        # Connect Database
+        my_db = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME
+        )
+        my_cursor = my_db.cursor(dictionary=True)
+        sql = """
+            UPDATE flowers
+            SET flower_name = %s,
+            lat_num = %s,
+            long_num = %s,
+            place = %s,
+            detail = %s
+            WHERE id = %s
+        """
+        val = (flower_name, lat_num, long_num, place, detail, flower_id)
+        my_cursor.execute(sql, val)
+        my_db.commit()
+
+        session['alert_status'] = "success"
+        session['alert_message'] = "Already Updated!"
+        return redirect("/")
+     else:
+        session['alert_status'] = "fail"
+        session['alert_message'] = "Something went wrong!"
+        return redirect("/")
+     
+@app.route("/delete/<flower_id>", methods=["GET"])
+def delete(flower_id):
+     # Connect Database
+        my_db = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME
+        )
+        my_cursor = my_db.cursor(dictionary=True)
+        sql = "DELETE FROM flowers WHERE id = " + flower_id
+        my_cursor.execute(sql,)
+        my_db.commit()
+
+        session['alert_status'] = "success"
+        session['alert_message'] = "Already Deleted!"
+        return redirect("/")
+
 
 if __name__ == "__main__":
    # app.run() # production
